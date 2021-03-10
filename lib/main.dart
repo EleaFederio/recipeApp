28956db6b44a4b3efaa10e1fbfd7be7b2e878534
EleazarFederio <eleazarfederio_@outlook.com>
2021-03-10 23:18:39 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:mealApp/screens/categories_screen.dart';
+import 'package:mealApp/screens/category_meals.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Color.fromRGBO(237, 229, 204, 1),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        canvasColor: Color.fromRGBO(237, 229, 204, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(
+              color: Color.fromRGBO(0, 0, 0, 1),
+            ),
+            body2: TextStyle(
+              color: Color.fromRGBO(100, 136, 19, 1),
+            ),
+            title: TextStyle(
+                fontSize: 24,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold)),
+      ),
+      // home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen()
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals App'),
+      ),
+      body: Center(
+        child: Text('Navigation Time'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/category_meals.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:mealApp/dummy_data.dart';
+import 'package:mealApp/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    // filter by categories where = to seleted category id
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          // return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
